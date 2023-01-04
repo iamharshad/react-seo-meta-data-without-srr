ee5cacd80bd6fb8a2d6b38e399c4ea84c7da0653
Harshad Prajapati <iamharshad.prajapati@gmail.com>
2023-01-04 12:07:32 +0530

    seq change at top

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const indexPath = path.resolve(__dirname, "build", "index.html");
 
+// static resources should just be served as they are
+app.use(express.static(path.resolve(__dirname, "build"), { maxAge: "30d" }));
+
 // listening...
 app.listen(PORT, (error) => {
   if (error) {
@@ -13,9 +16,6 @@ app.listen(PORT, (error) => {
   }
   console.log("listening on " + PORT + "...");
 });
-
-// static resources should just be served as they are
-app.use(express.static(path.resolve(__dirname, "build"), { maxAge: "30d" }));
 
 // here we serve the index.html page
 app.get("/*", (req, res, next) => {
